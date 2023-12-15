a3d315953702df25a503bc01acf64d881be3fc3a
Eduardo <ehernandezvilla@gmail.com>
2023-12-15 14:03:18 -0300

    code: adding track functionality

--- src/components/Track.jsx
+++ src/components/Track.jsx
@@ -1,19 +1,19 @@
-import PropTypes from "react";
+import PropTypes from "prop-types";
 
 
 function Track({ data }) {
     return (
         <div className="Song">
-        <div className="SongInformation">
-            <h3>{data.name}</h3>
-            <p>artist | album</p>
-        </div>
+            <div>
+                <h3>{data.name}</h3>
+                <p>{data.artist} | {data.album}</p>
+            </div>
         </div>
     );
 }
 
 Track.propTypes = {
-    data: PropTypes.object.isRequired,
+    data: PropTypes.object,
 };
 
 export default Track;
--- src/components/TrackList.jsx
+++ src/components/TrackList.jsx
@@ -1,10 +1,29 @@
-function TrackList() {
-    return (
-        <>
-        <h3>TrackList component</h3>
-        <p> Song X </p>
-        </>
-    );
+import PropTypes from "prop-types";
+import Track from "./Track";
+
+function TrackList({ data }) {
+  return (
+    <>
+        <div>
+        {data.songs.map((song) => (
+           <Track key={song.id} data={song} />
+         ))}
+      </div>
+    </>
+  );
 }
+
+TrackList.propTypes = {
+    data: PropTypes.shape({
+        songs: PropTypes.arrayOf(
+            PropTypes.shape({
+                id: PropTypes.string,
+                name: PropTypes.string,
+                album: PropTypes.string,
+                artist: PropTypes.string
+            })
+        )
+    }).isRequired,
+};
 
 export default TrackList;
