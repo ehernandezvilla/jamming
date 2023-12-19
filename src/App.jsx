3589ad2b603f498a676c15bea08de6f35faa6b88
Eduardo <ehernandezvilla@gmail.com>
2023-12-18 21:59:57 -0300

    style: fixing separator style

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,47 @@
-import { useState } from 'react';
-import './App.css'
-import Playlist from './components/Playlist'
-import SearchBar from './components/SearchBar'
-import SearchResults from './components/SearchResults';
-import songs from './assets/utils/songs.json'
-
+import { useState } from "react";
+import "./App.css";
+import Playlist from "./components/Playlist";
+import SearchBar from "./components/SearchBar";
+import SearchResults from "./components/SearchResults";
+import songs from "./assets/utils/songs.json";
 
 function App() {
   const [selectedSongs, setSelectedSongs] = useState([]);
   const [searchResults, setSearchResults] = useState({ songs: [] });
- 
+
+  const handleAddSong = (song) => {
+    if (!selectedSongs.some((selectedSong) => selectedSong.id === song.id)) {
+      setSelectedSongs([...selectedSongs, song]);
+    }
+  };
 
   const handleSearch = (searchTerm) => {
-    const filteredSongs = songs.songs.filter(song => 
-      song.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      song.artist.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      song.album.toLowerCase().includes(searchTerm.toLowerCase())
+    const filteredSongs = songs.songs.filter(
+      (song) =>
+        song.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        song.artist.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        song.album.toLowerCase().includes(searchTerm.toLowerCase())
     );
     setSearchResults({ songs: filteredSongs });
   };
 
-
   return (
     <>
       <h1>Dummy Spotify app</h1>
-      <SearchBar onSearch ={handleSearch} />
-      <div className='BodyContainer'>
+      <SearchBar onSearch={handleSearch} />
+      <div className="BodyContainer">
+        <div className="SeparatorContainer">
+          <SearchResults 
+            data={searchResults} 
+            onAddSong={handleAddSong}
+          />
         </div>
-        <div className='SeparatorContainer'>
-          <SearchResults data={searchResults} 
-          onAddSong={(song) => setSelectedSongs([...selectedSongs, song])} 
-          /> 
-          <Playlist selectedSongs={selectedSongs}/>
+        <div className="SeparatorContainer">
+          <Playlist selectedSongs={selectedSongs} />
         </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
